4812623bbdb06e95af484cf7d659cf305831c947
ArV <aryanrajvaidya@gmail.com>
2021-04-04 11:48:47 +0530

    adding files so that the repo doesn't feel too empty

---
+++ TicTacToe/script.js
@@ -0,0 +1,16 @@
+a1 = document.getElementById('a1').innerHTML
+b1 = document.getElementById('b1').innerHTML
+c1 = document.getElementById('c1').innerHTML
+a2 = document.getElementById('a2').innerHTML
+b2 = document.getElementById('b2').innerHTML
+c2 = document.getElementById('c2').innerHTML
+a3 = document.getElementById('a3').innerHTML
+b3 = document.getElementById('b3').innerHTML
+c3 = document.getElementById('c3').innerHTML
+
+function initialize(){
+    a1 = 'X'
+    a2 = 'X'
+    a3 = 'X'
+
+}
